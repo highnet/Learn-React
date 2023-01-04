122cae08ebc4972c8535c8953fc56b81ff7adbea
joaquin telleria <joaquintelleria@gmail.com>
2023-01-04 19:39:30 +0100

    ,

--- prototype b/counter-app/src/components/counter.jsx
+++ prototype b/counter-app/src/components/counter.jsx
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 0,
+    }
+
+    styles = {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+
     render() { 
-        return (<h1>Hello World</h1>);
+        return (
+        <React.Fragment>
+            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button>
+        </React.Fragment>
+        );
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary"
+        return classes;
     }
 }
  
