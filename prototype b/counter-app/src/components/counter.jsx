aca092a9c652445fa8c35c491a5ce79c92dcb629
joaquin telleria <joaquintelleria@gmail.com>
2023-01-06 14:20:00 +0100

    .

--- prototype b/counter-app/src/components/counter.jsx
+++ prototype b/counter-app/src/components/counter.jsx
@@ -8,20 +8,21 @@ class Counter extends Component {
 
     renderTags(){
         if(this.state.tags.length === 0) return <p>There are no tags</p>;
-
         return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-
     }
 
-    handleIncrement(){
-        console.log('Increment Clicked');
+    handleIncrement = product => {
+        console.log(product.count);
+        this.setState({count: this.state.count + 1});
     }
+
+
 
     render() { 
         return (
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+            <button onClick={() => this.handleIncrement({product: 1})} className='btn btn-secondary btn-sm'>Increment</button>
             {this.renderTags()}
         </div>
         );
