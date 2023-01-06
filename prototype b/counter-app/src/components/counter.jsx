c6318c4361ee2067637a722d086d4ce96ebc70c1
joaquin telleria <joaquintelleria@gmail.com>
2023-01-06 13:53:47 +0100

    m

--- prototype b/counter-app/src/components/counter.jsx
+++ prototype b/counter-app/src/components/counter.jsx
@@ -3,19 +3,27 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
+        tags: ['tag', 'tag2', 'tag3']
     }
 
-    styles = {
-        fontSize: 20,
-        fontWeight: 'bold'
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There are no tags</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+
+    }
+
+    handleIncrement(){
+        console.log('Increment Clicked');
     }
 
     render() { 
         return (
-        <React.Fragment>
-            <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
-        </React.Fragment>
+        <div>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+            {this.renderTags()}
+        </div>
         );
     }
 
